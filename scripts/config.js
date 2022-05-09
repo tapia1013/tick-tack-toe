4eba8e59a90e5188c7beea08cb007951031c8c7e
tapia1013 <manbearpig1013@gmail.com>
2022-05-09 15:42:07 -0700

    handling form submission beginning

--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,15 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
 }
+
+function savePlayerConfig(e) {
+  e.preventDefault();
+
+  // grab the value
+  const formData = new FormData(e.target);
+
+  // gets the name attr in input
+  const enteredPlayername = formData.get('playername');
+
+  console.log(enteredPlayername);
+}
