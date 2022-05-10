45f8fdd48af6e67058e1808290e3f05ef497d5d3
tapia1013 <manbearpig1013@gmail.com>
2022-05-09 17:55:38 -0700

    managing game rounds who is current player and field selection

--- scripts/game.js
+++ scripts/game.js
@@ -4,5 +4,33 @@ function startNewGame() {
     return;
   }
 
+  activePlayerNameElement.textContent = players[activePlayer].name;
+
   gameAreaElement.style.display = 'block';
+}
+
+
+// to swith players when already clicked box
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+
+// We're looping through players and using this function eventListener
+function selectGameField(e) {
+  // check to see if gap is clicked
+  if (e.target.tagName !== 'LI') {
+    return;
+  }
+
+  e.target.textContent = players[activePlayer].symbol; // players[0]
+  e.target.classList.add('disabled');
+
+  switchPlayer()
 }
