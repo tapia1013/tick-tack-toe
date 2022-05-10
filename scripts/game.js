e9412155ee70075070abf9bc9d72a838979b6080
tapia1013 <manbearpig1013@gmail.com>
2022-05-09 19:09:34 -0700

    checking for a winner or a draw

--- scripts/game.js
+++ scripts/game.js
@@ -10,6 +10,7 @@ function startNewGame() {
 }
 
 
+
 // to swith players when already clicked box
 function switchPlayer() {
   if (activePlayer === 0) {
@@ -47,7 +48,67 @@ function selectGameField(e) {
 
   // Update gameData fields
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  console.log(gameData);
+  // console.log(gameData);
+
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+
+  // increment to check if its a draw in checkForGameOver
+  currentRound++;
 
   switchPlayer()
+}
+
+
+
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // Checking Columns for equality
+  for (i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Diaganol: Top Left to Bottom Right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0]
+  }
+
+  // Diaganol: Bottom Left to Top Right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0]
+  }
+
+
+  // check if its a draw
+  if (currentRound === 9) {
+    return -1;
+  }
+
+
+  return 0;
 }
