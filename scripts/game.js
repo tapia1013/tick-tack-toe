06b7a39a2196b84ab6d95c8a5a1f734205855bdb
tapia1013 <manbearpig1013@gmail.com>
2022-05-09 18:28:04 -0700

    Tracking Selected Fields On The Game Board

--- scripts/game.js
+++ scripts/game.js
@@ -29,8 +29,25 @@ function selectGameField(e) {
     return;
   }
 
-  e.target.textContent = players[activePlayer].symbol; // players[0]
-  e.target.classList.add('disabled');
+  const selectedField = e.target;
+
+  // get gameData fields.. -1 is to start at 0 index and converts to Number
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  // if box already selected then value is either 1 or 2 and we cant click on it anymore cause its already been asssigned
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Please Select and Empty Field')
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol; // players[0]
+  selectedField.classList.add('disabled');
+
+
+  // Update gameData fields
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
 
   switchPlayer()
 }
